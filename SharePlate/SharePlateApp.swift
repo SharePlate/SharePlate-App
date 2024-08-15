25cffff1e89cccac7cf6048bfe604ce428cc4cdd
alimusawa313 <90315924+alimusawa313@users.noreply.github.com>
2024-08-15 17:53:12 +0700

    Integrate Firebase

--- SharePlate/SharePlateApp.swift
+++ SharePlate/SharePlateApp.swift
@@ -6,19 +6,35 @@
 //
 import netfox
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil)
+    -> Bool {
+        return true
+    }
+}
 
 @main
 struct SharePlateApp: App {
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    @StateObject var authManager: AuthManager
+    
     init() {
-        #if DEBUG
-        NFX.sharedInstance().start()
-        #endif
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        
+        let authManager = AuthManager()
+        _authManager = StateObject(wrappedValue: authManager)
     }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authManager)
         }
     }
 }
